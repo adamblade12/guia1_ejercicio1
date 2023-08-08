9fef84bd9d99b7416eb5cb93f9cbd7049419db2d
ArielGR2 <arielgromera@gmail.com>
2023-08-08 12:21:05 -0300

    Cree la clase Ariel

---
+++ ejercicio1/src/ejercicio1/Ariel.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+/**
+ * @author Ariel
+ */
+
+public class Ariel {
+    private int a = 0;
+    private String  b = "Hola";
+    
+    public void saludar(){
+        System.out.println(b);
+    }
+    
+}
